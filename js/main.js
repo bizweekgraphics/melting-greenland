ea89e83e49a0d5c4718a437f18d1a15d900321eb
dyep49 <dyep49@gmail.com>
2014-04-29 18:30:10 -0400

    add year search

--- js/main.js
+++ js/main.js
@@ -1,62 +1,89 @@
-var width = 1000
-var height = 1000
+$('#year-submit').click(function() {
+	$('circle').remove()
+	var year = $('#year').val()
+	appendMap(year)
+	return false;
+})
+
+
+
+// var maxMeltArray = []
+
+// var calcMax = function() {
+// 	for (var i=1973;i<2014;i++) {
+// 		var dfd = new jQuery.Deferred();
+// 		var max = d3.max(greenland, function(point) {
+// 			return point["year" + year]
+// 		})
+// 		dfd.resolve(max)
+// 	}
+// 	return dfd.promise()
+// }
+
+// $.when(calcMax() ).then(
+// 	function(status) {
+// 		maxMeltArray
+// 	})
+
+
+var width = 1500
+var height = 900
 
 var svg = d3.select('body').append('svg')
 	.attr('width', width)
 	.attr('height', height)
+	.attr('id', 'chart')
 
-var latMin = d3.min(greenland, function(point) {
-	return point.latitude
-})
-
-var lngMin = d3.min(greenland, function(point) {
-	return point.longitude
-})
-
-var latMax = d3.max(greenland, function(point) {
-	return point.latitude
-})
-
-var lngMax = d3.max(greenland, function(point) {
-	return point.longitude
-})
-
-var meltMax = d3.max(greenland, function(point) {
-	return point["year 2000"]
-})
-
-var meltX = d3.scale.linear() 
-	.domain([0, meltMax])
-	.range(["white", "red"])
-
-var x = d3.scale.linear()
-	.domain([lngMin, lngMax])
-	.range([0, width])
-
-var y = d3.scale.linear()
-	.domain([latMin, latMax])
-	.range([height, 0])
-
-var meltProjection = d3.select('svg')
-	.selectAll('circle')
-	.data(greenland)
-
-meltProjection.enter()
-	.append('circle')
-	.attr('cx', function(d){
-		return x(d.longitude)
-	})
-	.attr('cy', function(d){
-		return y(d.latitude)
-	})
-	.attr('r', 3)
-	.style('fill', function(d) {
-		return meltX(d["year 2000"])
+var appendMap = function(year) {
+	var latMin = d3.min(greenland, function(point) {
+		return point.latitude
 	})
 
+	var lngMin = d3.min(greenland, function(point) {
+		return point.longitude
+	})
 
+	var latMax = d3.max(greenland, function(point) {
+		return point.latitude
+	})
 
+	var lngMax = d3.max(greenland, function(point) {
+		return point.longitude
+	})
 
+	var meltMax = d3.max(greenland, function(point) {
+		return point["year " + year]
+	})
+
+	var meltX = d3.scale.linear() 
+		.domain([0, meltMax])
+		.range(["white", "red"])
+
+	var x = d3.scale.linear()
+		.domain([lngMin, lngMax])
+		.range([0, width])
+
+	var y = d3.scale.linear()
+		.domain([latMin, latMax])
+		.range([height, 0])
+
+	var meltProjection = d3.select('svg')
+		.selectAll('circle')
+		.data(greenland)
+
+	meltProjection.enter()
+		.append('circle')
+		.attr('cx', function(d){
+			return x(d.longitude)
+		})
+		.attr('cy', function(d){
+			return y(d.latitude)
+		})
+		.attr('r', 3)
+		.style('fill', function(d) {
+			return meltX(d["year " + year])
+		})
+}
 
 
 
