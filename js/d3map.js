e0f83b07ff47d51b4bba410de5239d4bc0ceab55
dyep49 <dyep49@gmail.com>
2014-05-02 11:08:32 -0400

    removed commented out code

--- js/d3map.js
+++ js/d3map.js
@@ -89,10 +89,6 @@ var appendMap = function(year) {
 				return tickScale(days)
 			})
 		})
-		// .style('fill', 'black')
-		// .transition()
-		// .duration(500)
-		// .delay(100)
 		.style('fill', function(d) {
 			if(d["year " + year] === 0){
 				return backgroundColor
@@ -100,43 +96,6 @@ var appendMap = function(year) {
 				return meltX(d["year " + year])			
 			}
 		})
-
-
-	// var textProjection = d3.select('svg')
-	// 	.selectAll('text')
-	// 	.data([1])
-
-	//append year text to svg
-	// textProjection.enter()
-	// 	.append('text')
-	// 	.text(year)
-	// 	.attr('width', 400)
-	// 	.attr('height', 200)
-	// 	.attr('x', 180)
-	// 	.attr('y', 50)
-	// 	.attr('id', 'text-year')
-	// 	.style('fill', 'white')
-	// 	.style('font-size', 55)
-
-	//append sun to svg
-	// d3.select('svg')
-	// 	.append('circle')
-	// 	.attr('cx', 700)
-	// 	.attr('cy', 90)
-	// 	.attr('r', 80)
-	// 	.attr('id', 'sun')
-	// 	.attr('class', 'bright-fill')
-	// 	.style('fill', 'yellow')
-
-	// d3.select('svg')
-	// 	.append('text')
-	// 	.text('test')
-	// 	.attr('width', 100)
-	// 	.attr('height', 150)
-	// 	.attr('x', 685)
-	// 	.attr('y', 100)
-	// 	.attr('id', 'day-text')
-	// 	.style('font-size', '2em')
 
 	//append number of melt days to svg
 	d3.select('svg')
@@ -149,12 +108,6 @@ var appendMap = function(year) {
 		.attr('id', 'day-text')
 		.style('font-size', '3em')
 		.style('fill', 'white')
-
-	// var years = [1979, 1995, 2012]
-
-	// var buttonProjection = d3.select('svg')
-	// 	.selectAll('.year-button')
-	// 	.data(years)
 
 	//appends key div to svg
 	d3.select('svg')
@@ -198,64 +151,11 @@ var appendMap = function(year) {
 		.attr('width', 300)
 		.attr('height', 100)
 		.attr('x', 2)
-		.attr('y', 300)
+		.attr('y', 568)
 		.attr('id', 'key-tick')
 		.append('xhtml:p')
 		.text('—')
 		.style('font-size', '5em')
-
-
-
-
-	//append year click buttons to svg
-	// buttonProjection.enter()
-	// 	.append('foreignObject')
-	// 	.attr('width', 200)
-	// 	.attr('height', 200)
-	// 	.attr('x', 10)
-	// 	.attr('y', function(d){
-	// 		var y = years.indexOf(d)
-	// 		return 200 + (100 * y)
-	// 	})
-	// 	.append('xhtml:body')
-	// 	.append('xhtml:ul')
-	// 	.attr('class', 'year-ul')
-	// 	.append('xhtml:li')
-	// 	.on('click', function() {
-	// 		$('#day-text').text('')
-	// 		$('#arrow').animate({left: "0%"}, 1000)
-
-	// 		//select button inside list element
-	// 		var button = $(this).children()
-	// 		$('button').removeClass()
-
-	// 		var liYear = this.textContent
-	// 		if($('circle').length === 0) {
-	// 			appendMap(liYear)	
-	// 		} else {
-	// 			updateProjection(liYear)
-	// 		}
-
-	// 		if(liYear === "1979"){
-	// 			button.addClass('bright')
-	// 		} else if(liYear == "1995") {
-	// 			button.addClass('brighter')
-	// 		} else {
-	// 			button.addClass('brightest')
-	// 		}
-	// 	})
-	// 	.append('xhtml:button')
-	// 	.attr('width', '200px')
-	// 	.attr('class', function(d){
-	// 		if(d === 1979){
-	// 			return 'bright'
-	// 		}
-	// 	})
-	// 	.style('color', 'black')
-	// 	.text(function(d){
-	// 		return d
-	// 	})
-
 }
 
 var updateProjection = function(year) {
@@ -283,40 +183,6 @@ var updateProjection = function(year) {
 				return meltX(d["year " + year])			
 			}
 		})
-
-	d3.selectAll('#text-year')
-		.text(year)
-
-	// d3.select('#sun')
-	// 	.transition()
-	// 	.duration(1000)
-	// 	.delay(100)
-	// 	.style('fill', function() {
-	// 		var text = d3.select('#text-year').text()
-	// 		$('#sun').attr('class', '')
-	// 		if(text === "1979"){
-	// 			$('#sun').attr('class', 'bright-fill')
-	// 			return 'yellow'
-	// 		} else if(text==="1995") {
-	// 			$('#sun').attr('class', 'brighter-fill')
-	// 			return 'orange'
-	// 		} else {
-	// 			$('#sun').attr('class', 'brightest-fill')
-	// 			return 'red'
-	// 		}
-	// 	})
-
-	// var text = d3.select('#text-year').text()
-	// 	if(text === "1979"){
-	// 		$('#sun').attr('class', 'bright-fill')
-	// 		return 'yellow'
-	// 	} else if(text==="1995") {
-	// 		$('#sun').attr('class', 'brighter-fill')
-	// 		return 'orange'
-	// 	} else {
-	// 		$('#sun').attr('class', 'brightest-fill')
-	// 		return 'red'
-	// 	}
 }
 
 
